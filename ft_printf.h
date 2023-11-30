ce17659f6ec53e12721500a3665c2d4c4fa7440b
Thibaud Mouche <tmouche@z4r3p1.42lyon.fr>
2023-11-30 15:42:44 +0100

    normed

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: tmouche <tmouche@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 14:13:41 by tmouche           #+#    #+#             */
-/*   Updated: 2023/11/30 15:30:01 by tmouche          ###   ########.fr       */
+/*   Updated: 2023/11/30 15:39:17 by tmouche          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ ssize_t	ft_putstr(char *str);
 int		ft_printf(const char *prompt, ...);
 size_t	ft_strlen(const char *s);
 ssize_t	ft_putstr_stoped(char *str);
-int	ft_counter(int *total, int len);
+int		ft_counter(int *total, int len);
 
 #endif
